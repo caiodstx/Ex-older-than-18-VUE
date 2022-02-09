2c0d2d70e2a18100087d025e7788bfb7b6cdb92f
Caio Da Silva <caiodasilvatx@gmail.com>
2022-02-09 10:52:56 +0100

    created a component to check age

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <main>
+      <AgeCheck />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AgeCheck from "./components/AgeCheck";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AgeCheck,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/AgeCheck.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card elevation="24">
+    <form>
+      <v-text-field
+        v-model="name"
+        :error-messages="nameErrors"
+        :counter="10"
+        label="Name"
+        required
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-text-field
+        v-model="age"
+        :error-messages="ageErrors"
+        label="Your Age"
+        required
+        @input="$v.age.$touch()"
+        @blur="$v.age.$touch()"
+      ></v-text-field>
+      <v-card-actions class="d-flex">
+        <v-btn @click="submit"> submit </v-btn>
+        <v-btn @click="clear"> clear </v-btn>
+      </v-card-actions>
+    </form>
+  </v-card>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+
+  name: "AgeCheck",
+
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    age: { required },
+  },
+
+  data: () => ({
+    name: "",
+    age: "",
+  }),
+
+  computed: {
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("Name must be at most 10 characters long");
+      !this.$v.name.required && errors.push("Name is required.");
+      return errors;
+    },
+    ageErrors() {
+      const errors = [];
+      if (!this.$v.age.$dirty) return errors;
+      !this.$v.age.age && errors.push("Must be valid age");
+      !this.$v.age.required && errors.push("Age is required");
+      return errors;
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.age = "";
+    },
+  },
+};
+</script>
